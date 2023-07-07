ab992c8f05a5ed9898ebcca4115611ef25a06634
ViniciusSilvaDeOliveira <tiviniciusoliveira@gmail.com>
2023-07-07 16:12:35 -0300

    atualização

--- src/main/java/med/voll/api/controller/AgendamentoController.java
+++ src/main/java/med/voll/api/controller/AgendamentoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/agendamento")
+@RequestMapping("agendamentos")
 public class AgendamentoController {
 
     @Autowired
